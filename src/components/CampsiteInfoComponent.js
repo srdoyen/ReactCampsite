7f4b2a60f5e1572621326d41b6b2d303e2beb31f
Scott Doyen <srdoyen@gmail.com>
2020-04-27 17:01:04 -0700

    Presentation and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,11 +2,6 @@ import React, { Component } from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
 class CampsiteInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   renderCampsite(campsite) {
     return (
       <div className="col-md-5 m-1">
@@ -51,9 +46,11 @@ class CampsiteInfo extends Component {
   render() {
     if (this.props.campsite) {
       return (
-        <div className="row">
-          {this.renderCampsite(this.props.campsite)}
-          {this.renderComments(this.props.campsite.comments)}
+        <div className="container">
+          <div className="row">
+            {this.renderCampsite(this.props.campsite)}
+            {this.renderComments(this.props.campsite.comments)}
+          </div>
         </div>
       );
     } else {
