9bbf1880ae8819ae06b71b729d5ed7a41b80bbdf
Scott Doyen <srdoyen@gmail.com>
2020-05-11 22:06:33 -0700

    redux THUNK

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -10,6 +10,7 @@ import {
   Label,
 } from "reactstrap";
 import { Control, LocalForm, Errors } from "react-redux-form";
+import { Loading } from "./LoadingComponent";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -157,6 +158,28 @@ function RenderComments({ comments, addComment, campsiteId }) {
   return <div></div>;
 }
 function CampsiteInfo(props) {
+  if (props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  }
+
+  if (props.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h4>{props.errMess}</h4>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   if (props.campsite) {
     return (
       <div className="container">
